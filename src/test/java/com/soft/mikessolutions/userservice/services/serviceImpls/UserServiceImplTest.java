2fb8201af554899bd11c10b23838e4f0b44a5ffd
Michal Badura <badura.mike@gmail.com>
2019-01-20 18:57:50 +0100

    Add tests for UserServiceImpl save(), delete(), deleteById().

--- src/test/java/com/soft/mikessolutions/userservice/services/serviceImpls/UserServiceImplTest.java
+++ src/test/java/com/soft/mikessolutions/userservice/services/serviceImpls/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.soft.mikessolutions.userservice.services.serviceImpls;
 
+import com.soft.mikessolutions.userservice.entities.User;
 import com.soft.mikessolutions.userservice.exceptions.user.UserNotFoundException;
 import com.soft.mikessolutions.userservice.services.UserService;
 import org.junit.Assert;
@@ -28,18 +29,58 @@ public class UserServiceImplTest {
 
     @Test
     public void shouldFindUserById2() {
-        Assert.assertEquals("grazyna.idzzesz@warbud.pl",userService.findById(2L).getEmail());
+        Assert.assertEquals("grazyna.idzzesz@warbud.pl", userService.findById(2L).getEmail());
     }
 
     @Test
-    public void shouldThrowUserNotFoundExceptionForId100() {
+    public void shouldThrowUserNotFoundExceptionForNonExistingId() {
         //GIVEN
         //WHEN
-        Long id = 100L;
+        Long id = (long) (userService.findAll().size() + 1);
         //THEN
         expectedException.expect(UserNotFoundException.class);
         expectedException.expectMessage("User with {id} = " + id + " not found.");
         userService.findById(id);
     }
+
+    @Test
+    public void shouldSaveNewUser() {
+        //GIVEN
+        int preSaveUserCount = userService.findAll().size();
+        User newUser = new User();
+        //WHEN
+        userService.save(newUser);
+        int postSaveUserCount = userService.findAll().size();
+        //THEN
+        Assert.assertEquals(1,postSaveUserCount - preSaveUserCount);
+    }
+
+
+    @Test
+    public void shouldDeleteUser() {
+        //GIVEN
+        User newUser = new User();
+        userService.save(newUser);
+        int preDeleteUserCount = userService.findAll().size();
+        //WHEN
+        userService.delete(newUser);
+        int postDeleteUserCount = userService.findAll().size();
+        //THEN
+        Assert.assertEquals(1,preDeleteUserCount - postDeleteUserCount);
+    }
+
+    @Test
+    public void shouldDeleteUserById1() {
+        //GIVEN
+        User newUser = new User();
+        userService.save(newUser);
+        int preDeleteUserCount = userService.findAll().size();
+        //WHEN
+        userService.deleteById(newUser.getId());
+        int postDeleteUserCount = userService.findAll().size();
+        //THEN
+        Assert.assertEquals(1,preDeleteUserCount - postDeleteUserCount);
+    }
+
 
 }
